88d8850cec78e6562e41fff70b2ecae32e611d68
justinkrojos <justinkrojos@gmail.com>
2022-04-24 18:06:54 +1200

    created custom config hook

---
+++ src/config/useConfig.jsx
@@ -0,0 +1,59 @@
+import { useState, useEffect } from "react";
+
+// Copied over from test brief
+const configuration = {
+  columns: [
+    {
+      // First column
+      size: 1, // The size of the column
+      heading: "General Pokemon information",
+      widgets: [
+        {
+          title: "Pokemon colours",
+          subtitle: "List of all the colours",
+          type: "list",
+          api: "https://pokeapi.co/api/v2/pokemon-color/",
+        },
+        {
+          title: "Pokemon Gender",
+          subtitle: "List of all the genders",
+          type: "list",
+          api: "https://pokeapi.co/api/v2/gender/",
+        },
+      ],
+    },
+    {
+      // Second column
+      size: 2, // The size of the column
+      heading: "Pokemon counts",
+      widgets: [
+        {
+          title: "Genderless count",
+          type: "number",
+          api: "https://pokeapi.co/api/v2/gender/3/",
+        },
+        {
+          title: "Female count",
+          type: "number",
+          api: "https://pokeapi.co/api/v2/gender/1/",
+        },
+        {
+          title: "Male count",
+          type: "number",
+          api: "https://pokeapi.co/api/v2/gender/2/",
+        },
+      ],
+    },
+  ],
+};
+
+// Custom hook to listen for changes in configuration data
+export function useConfig() {
+  const [config, setConfig] = useState(configuration);
+
+  useEffect(() => {
+    setConfig(configuration);
+  }, [config]);
+
+  return config;
+}
